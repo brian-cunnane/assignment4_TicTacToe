640306b8bd92788cb0b40f6dfed4df31bc6f35be
Brian Cunnane <g00174469@gmit.ie>
2015-10-12 22:53:24 +0100

    added game over message. added ability to check for winner. Long and complicated if statement could be improved. bug exists where X will be declared winner in event of a draw.

--- TicTacToeServer.java
+++ TicTacToeServer.java
@@ -7,277 +7,310 @@ import java.io.*;
 import javax.swing.*;
 
 public class TicTacToeServer extends JFrame {
-   private char[] board;           
-   private JTextArea outputArea;
-   private Player[] players;
-   private ServerSocket server;
-   private int currentPlayer;
-   private final int PLAYER_X = 0, PLAYER_O = 1;
-   private final char X_MARK = 'X', O_MARK = 'O';
+    private char[] board;
+    private JTextArea outputArea;
+    private Player[] players;
+    private ServerSocket server;
+    private int currentPlayer;
+    private final int PLAYER_X = 0, PLAYER_O = 1;
+    private final char X_MARK = 'X', O_MARK = 'O';
 
-   // set up tic-tac-toe server and GUI that displays messages
-   public TicTacToeServer()
-   {
-      super( "Tic-Tac-Toe Server" );
+    public int count = 0;
 
-      board = new char[ 9 ]; 
-      players = new Player[ 2 ];
-      currentPlayer = PLAYER_X;
- 
-      // set up ServerSocket
-      try {
-         server = new ServerSocket( 12345, 2 );
-      }
+    // set up tic-tac-toe server and GUI that displays messages
+    public TicTacToeServer()
+    {
+        super( "Tic-Tac-Toe Server" );
 
-      // process problems creating ServerSocket
-      catch( IOException ioException ) {
-         ioException.printStackTrace();
-         System.exit( 1 );
-      }
+        board = new char[ 9 ];
+        players = new Player[ 2 ];
+        currentPlayer = PLAYER_X;
 
-      // set up JTextArea to display messages during execution
-      outputArea = new JTextArea();
-      getContentPane().add( outputArea, BorderLayout.CENTER );
-      outputArea.setText( "Server awaiting connections\n" );
+        // set up ServerSocket
+        try {
+            server = new ServerSocket( 12345, 2 );
+        }
 
-      setSize( 300, 300 );
-      setVisible( true );
-
-   } // end TicTacToeServer constructor
-
-   // wait for two connections so game can be played
-   public void execute()
-   {
-      // wait for each client to connect
-      for ( int i = 0; i < players.length; i++ ) {
-
-         // wait for connection, create Player, start thread
-         try {
-            players[ i ] = new Player( server.accept(), i );
-            players[ i ].start();
-         }
-
-         // process problems receiving connection from client
-         catch( IOException ioException ) {
+        // process problems creating ServerSocket
+        catch( IOException ioException ) {
             ioException.printStackTrace();
             System.exit( 1 );
-         }
-      }
+        }
 
-      // Player X is suspended until Player O connects. 
-      // Resume player X now.          
-      synchronized ( players[ PLAYER_X ] ) {
-         players[ PLAYER_X ].setSuspended( false );   
-         players[ PLAYER_X ].notify();
-      }
-  
-   }  // end method execute
-   
-   // utility method called from other threads to manipulate 
-   // outputArea in the event-dispatch thread
-   private void displayMessage( final String messageToDisplay )
-   {
-      // display message from event-dispatch thread of execution
-      SwingUtilities.invokeLater(
-         new Runnable() {  // inner class to ensure GUI updates properly
+        // set up JTextArea to display messages during execution
+        outputArea = new JTextArea();
+        getContentPane().add( outputArea, BorderLayout.CENTER );
+        outputArea.setText( "Server awaiting connections\n" );
 
-            public void run() // updates outputArea
-            {
-               outputArea.append( messageToDisplay );
-               outputArea.setCaretPosition( 
-                  outputArea.getText().length() );
+        setSize( 300, 300 );
+        setVisible( true );
+
+    } // end TicTacToeServer constructor
+
+    // wait for two connections so game can be played
+    public void execute()
+    {
+        // wait for each client to connect
+        for ( int i = 0; i < players.length; i++ ) {
+
+            // wait for connection, create Player, start thread
+            try {
+                players[ i ] = new Player( server.accept(), i );
+                players[ i ].start();
             }
 
-         }  // end inner class
+            // process problems receiving connection from client
+            catch( IOException ioException ) {
+                ioException.printStackTrace();
+                System.exit( 1 );
+            }
+        }
 
-      ); // end call to SwingUtilities.invokeLater
-   }
+        // Player X is suspended until Player O connects.
+        // Resume player X now.
+        synchronized ( players[ PLAYER_X ] ) {
+            players[ PLAYER_X ].setSuspended( false );
+            players[ PLAYER_X ].notify();
+        }
 
-   // Determine if a move is valid. This method is synchronized because 
-   // only one move can be made at a time.
-   public synchronized boolean validateAndMove( int location, int player )
-   {
-      boolean moveDone = false;
+    }  // end method execute
 
-      // while not current player, must wait for turn
-      while ( player != currentPlayer ) {
-         
-         // wait for turn
-         try {
-            wait();
-         }
+    // utility method called from other threads to manipulate
+    // outputArea in the event-dispatch thread
+    private void displayMessage( final String messageToDisplay )
+    {
+        // display message from event-dispatch thread of execution
+        SwingUtilities.invokeLater(
+                new Runnable() {  // inner class to ensure GUI updates properly
 
-         // catch wait interruptions
-         catch( InterruptedException interruptedException ) {
-            interruptedException.printStackTrace();
-         }
-      }
+                    public void run() // updates outputArea
+                    {
+                        outputArea.append( messageToDisplay );
+                        outputArea.setCaretPosition(
+                                outputArea.getText().length() );
+                    }
 
-      // if location not occupied, make move
-      if ( !isOccupied( location ) ) {
-  
-         // set move in board array
-         board[ location ] = currentPlayer == PLAYER_X ? X_MARK : O_MARK;
+                }  // end inner class
 
-         // change current player
-         currentPlayer = ( currentPlayer + 1 ) % 2;
+        ); // end call to SwingUtilities.invokeLater
+    }
 
-         // let new current player know that move occurred
-         players[ currentPlayer ].otherPlayerMoved( location );
+    // Determine if a move is valid. This method is synchronized because
+    // only one move can be made at a time.
+    public synchronized boolean validateAndMove( int location, int player )
+    {
+        boolean moveDone = false;
 
-         notify(); // tell waiting player to continue
+        // while not current player, must wait for turn
+        while ( player != currentPlayer ) {
 
-         // tell player that made move that the move was valid
-         return true; 
-      }
-
-      // tell player that made move that the move was not valid
-      else 
-         return false;
-
-   } // end method validateAndMove
-
-   // determine whether location is occupied
-   public boolean isOccupied( int location )
-   {
-      if ( board[ location ] == X_MARK || board [ location ] == O_MARK )
-          return true;
-      else
-          return false;
-   }
-
-   // place code in this method to determine whether game over 
-   public boolean isGameOver()
-   {
-      return false;  // this is left as an exercise
-   }
-
-   public static void main( String args[] )
-   {
-      TicTacToeServer application = new TicTacToeServer();
-      application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-      application.execute();
-   }
-
-   // private inner class Player manages each Player as a thread
-   private class Player extends Thread {
-      private Socket connection;
-      private DataInputStream input;
-      private DataOutputStream output;
-      private int playerNumber;
-      private char mark;
-      protected boolean suspended = true;
-
-      // set up Player thread
-      public Player( Socket socket, int number )
-      {
-         playerNumber = number;
-
-         // specify player's mark
-         mark = ( playerNumber == PLAYER_X ? X_MARK : O_MARK );
-
-         connection = socket;
-         
-         // obtain streams from Socket
-         try {
-            input = new DataInputStream( connection.getInputStream() );
-            output = new DataOutputStream( connection.getOutputStream() );
-         }
-
-         // process problems getting streams
-         catch( IOException ioException ) {
-            ioException.printStackTrace();
-            System.exit( 1 );
-         }
-
-      } // end Player constructor
-
-      // send message that other player moved
-      public void otherPlayerMoved( int location )
-      {
-         // send message indicating move
-         try {
-            output.writeUTF( "Opponent moved" );
-            output.writeInt( location );
-         }
-
-         // process problems sending message
-         catch ( IOException ioException ) { 
-            ioException.printStackTrace();
-         }
-      }
-
-      // control thread's execution
-      public void run()
-      {
-         // send client message indicating its mark (X or O),
-         // process messages from client
-         try {
-            displayMessage( "Player " + ( playerNumber == 
-               PLAYER_X ? X_MARK : O_MARK ) + " connected\n" );
- 
-            output.writeChar( mark ); // send player's mark
-
-            // send message indicating connection
-            output.writeUTF( "Player " + ( playerNumber == PLAYER_X ? 
-               "X connected\n" : "O connected, please wait\n" ) );
-
-            // if player X, wait for another player to arrive
-            if ( mark == X_MARK ) {
-               output.writeUTF( "Waiting for another player" );
-   
-               // wait for player O
-               try {
-                  synchronized( this ) {   
-                     while ( suspended )
-                        wait();  
-                  }
-               } 
-
-               // process interruptions while waiting
-               catch ( InterruptedException exception ) {
-                  exception.printStackTrace();
-               }
-
-               // send message that other player connected and
-               // player X can make a move
-               output.writeUTF( "Other player connected. Your move." );
+            // wait for turn
+            try {
+                wait();
             }
 
-            // while game not over
-            while ( ! isGameOver() ) {
+            // catch wait interruptions
+            catch( InterruptedException interruptedException ) {
+                interruptedException.printStackTrace();
+            }
+        }
 
-               // get move location from client
-               int location = input.readInt();
+        // if location not occupied, make move
+        if ( !isOccupied( location )) {
 
-               // check for valid move
-               if ( validateAndMove( location, playerNumber ) ) {
-                  displayMessage( "\nlocation: " + location );
-                  output.writeUTF( "Valid move." );
-               }
-               else 
-                  output.writeUTF( "Invalid move, try again" );
-            }         
+            // set move in board array
+            board[ location ] = currentPlayer == PLAYER_X ? X_MARK : O_MARK;
 
-            connection.close(); // close connection to client
+            // change current player
+            currentPlayer = ( currentPlayer + 1 ) % 2;
 
-         } // end try
+            // let new current player know that move occurred
+            players[ currentPlayer ].otherPlayerMoved( location );
 
-         // process problems communicating with client
-         catch( IOException ioException ) {
-            ioException.printStackTrace();
-            System.exit( 1 );
-         }
+            notify(); // tell waiting player to continue
+            count++;
+            // tell player that made move that the move was valid
+            return true;
+        }
 
-      } // end method run
+        // tell player that made move that the move was not valid
+        else
+            return false;
 
-      // set whether or not thread is suspended
-      public void setSuspended( boolean status )
-      {
-         suspended = status;
-      }
-   
-   } // end class Player
+    } // end method validateAndMove
+
+    // determine whether location is occupied
+    public boolean isOccupied( int location )
+    {
+        if ( board[ location ] == X_MARK || board [ location ] == O_MARK )
+            return true;
+        else
+            return false;
+    }
+
+    // place code in this method to determine whether game over
+    public boolean isGameOver()
+    {
+        if(count >= 9 || checkLocations())
+            return true;
+        else
+            return false;
+
+    }
+
+    private boolean checkLocations() {
+        if ((board[0] == X_MARK && board[1] == X_MARK && board[2] == X_MARK
+                ||board[3] == X_MARK && board[4] == X_MARK && board[5] == X_MARK
+                ||board[6] == X_MARK && board[7] == X_MARK && board[8] == X_MARK
+                ||board[0] == X_MARK && board[3] == X_MARK && board[6] == X_MARK
+                ||board[1] == X_MARK && board[4] == X_MARK && board[7] == X_MARK
+                ||board[2] == X_MARK && board[5] == X_MARK && board[8] == X_MARK
+                ||board[0] == X_MARK && board[4] == X_MARK && board[8] == X_MARK
+                ||board[2] == X_MARK && board[4] == X_MARK && board[6] == X_MARK)
+                )
+            return true;
+        else if (board[0] == O_MARK && board[1] == O_MARK && board[2] == O_MARK
+                ||board[3] == O_MARK && board[4] == O_MARK && board[5] == O_MARK
+                ||board[6] == O_MARK && board[7] == O_MARK && board[8] == O_MARK
+                ||board[0] == O_MARK && board[3] == O_MARK && board[6] == O_MARK
+                ||board[1] == O_MARK && board[4] == O_MARK && board[7] == O_MARK
+                ||board[2] == O_MARK && board[5] == O_MARK && board[8] == O_MARK
+                ||board[0] == O_MARK && board[4] == O_MARK && board[8] == O_MARK
+                ||board[2] == O_MARK && board[4] == O_MARK && board[6] == O_MARK)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main( String args[] )
+    {
+        TicTacToeServer application = new TicTacToeServer();
+        application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        application.execute();
+    }
+
+    // private inner class Player manages each Player as a thread
+    private class Player extends Thread {
+        private Socket connection;
+        private DataInputStream input;
+        private DataOutputStream output;
+        private int playerNumber;
+        private char mark;
+        protected boolean suspended = true;
+
+        // set up Player thread
+        public Player( Socket socket, int number )
+        {
+            playerNumber = number;
+
+            // specify player's mark
+            mark = ( playerNumber == PLAYER_X ? X_MARK : O_MARK );
+
+            connection = socket;
+
+            // obtain streams from Socket
+            try {
+                input = new DataInputStream( connection.getInputStream() );
+                output = new DataOutputStream( connection.getOutputStream() );
+            }
+
+            // process problems getting streams
+            catch( IOException ioException ) {
+                ioException.printStackTrace();
+                System.exit( 1 );
+            }
+
+        } // end Player constructor
+
+        // send message that other player moved
+        public void otherPlayerMoved( int location )
+        {
+            // send message indicating move
+            try {
+                output.writeUTF( "Opponent moved" );
+                output.writeInt( location );
+            }
+
+            // process problems sending message
+            catch ( IOException ioException ) {
+                ioException.printStackTrace();
+            }
+        }
+
+        // control thread's execution
+        public void run()
+        {
+            // send client message indicating its mark (X or O),
+            // process messages from client
+            try {
+                displayMessage( "Player " + ( playerNumber ==
+                        PLAYER_X ? X_MARK : O_MARK ) + " connected\n" );
+
+                output.writeChar( mark ); // send player's mark
+
+                // send message indicating connection
+                output.writeUTF( "Player " + ( playerNumber == PLAYER_X ?
+                        "X connected\n" : "O connected, please wait\n" ) );
+
+                // if player X, wait for another player to arrive
+                if ( mark == X_MARK ) {
+                    output.writeUTF( "Waiting for another player" );
+
+                    // wait for player O
+                    try {
+                        synchronized( this ) {
+                            while ( suspended )
+                                wait();
+                        }
+                    }
+
+                    // process interruptions while waiting
+                    catch ( InterruptedException exception ) {
+                        exception.printStackTrace();
+                    }
+
+                    // send message that other player connected and
+                    // player X can make a move
+                    output.writeUTF( "Other player connected. Your move." );
+                }
+
+                // while game not over
+                while ( ! isGameOver() ) {
+
+                    // get move location from client
+                    int location = input.readInt();
+
+                    // check for valid move
+                    if ( validateAndMove( location, playerNumber ) ) {
+                        displayMessage( "\nlocation: " + location );
+                        output.writeUTF( "Valid move." );
+                    }
+                    else
+                        output.writeUTF( "Invalid move, try again" );
+                }
+                output.writeUTF("GAME OVER" + currentPlayer + " wins!");
+                notify();
+                output.writeUTF("GAME OVER" + currentPlayer + " wins!");
+
+                connection.close(); // close connection to client
+
+            } // end try
+
+            // process problems communicating with client
+            catch( IOException ioException ) {
+                ioException.printStackTrace();
+                System.exit( 1 );
+            }
+
+        } // end method run
+
+        // set whether or not thread is suspended
+        public void setSuspended( boolean status )
+        {
+            suspended = status;
+        }
+
+    } // end class Player
 
 } // end class TicTacToeServer
 
